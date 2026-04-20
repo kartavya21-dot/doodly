a4798b97f3a04dedf1cc897e10b4b9fa996f18cf
kartavya21-dot <kartavyasharma0987@gmail.com>
2026-04-20 18:51:47 +0530

    For now room routes are done

--- Backend/routers/room.py
+++ Backend/routers/room.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+from schemas.user import UserBase
 from schemas.room import *
 from db.models import Room, User
 from db.session import SessionDep
@@ -26,8 +28,17 @@ async def get_rooms(session: SessionDep):
     rooms = session.exec(select(Room)).all()
     return rooms
 
+@router.get("/my", response_model=List[RoomResponse])
+async def get_my_rooms(session: SessionDep, user: User = Depends(get_current_user)):
+    db_user = session.get(User, user.username)
+    
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not Authenticated")
+    
+    return db_user.rooms
+
 @router.patch("/join-room", response_model=RoomResponse)
-async def join_room(session: SessionDep, room: RoomJoin, user: User = Depends(get_current_user)):
+async def join_room(session: SessionDep, room_id: RoomJoin, user: User = Depends(get_current_user)):
     
     db_room = session.get(Room, room.id)
 
@@ -50,3 +61,12 @@ async def join_room(session: SessionDep, room: RoomJoin, user: User = Depends(ge
     session.refresh(db_room)
         
     return db_room
+
+@router.get("/{room_id}/users", response_model=List[UserBase], dependencies=[Depends(get_current_user)])
+async def get_room_user(room_id: int, session: SessionDep):
+    db_room = session.get(Room, room_id)
+
+    if not db_room:
+        raise HTTPException(status_code=404, detail="Room not found")
+    
+    return db_room.users
